3357db0ea3a568bc1ec8c6663207b8846602dd66
VicS✩l =) <vsolano.ram@gmail.com>
2025-09-30 10:20:25 -0600

    Subí el código que hicimos durante la semana 3
    Aprendimos a definir variables, a insertar textos (qué no se ven cuando se ejecuta el programa pero si en el código) unilineales y multilineales

---
+++ Semana 3/Mi_cuarto_script.py
@@ -0,0 +1,12 @@
+# Este programa funcionaba con Python 3.13.7 >:3
+variable1 = 100
+variable2 = 150
+variable3 = variable1+variable2 # Esto no se va a usar ╰(*°▽°*)╯
+"""LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOOO
+OOOOOOOOOOOOO
+OOOOOOOOOOOOOOOOO
+OOOOOOOO
+OOOOOOOOOOOO
+OOOOOOOOOL"""
+print(variable3) 
