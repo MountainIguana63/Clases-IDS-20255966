7a9c81a438f5e4e34770ed6c81bc8ed661e69047
VicS✩l =) <vsolano.ram@gmail.com>
2025-10-07 17:20:12 -0600

    Clase 26/09/25 - IDS
    Añadí el archivo que trabajamos en la clase del jueves, 26 de septiembre del 2025.

---
+++ Semana3_IDS/26_09_25_IDS/Clase 03 - 03.py
@@ -0,0 +1,29 @@
+"""year = 2025 #Tipo de variable entera
+temperature = 28.5 #Tipo de variable float
+
+pi = 3.14159
+
+nota = 5.5       #Ejemplo de dato de tipo booleano
+aprobación = 6
+
+validación = nota == 5.5 #Con este comando buscamos asignarle el valor (=), a la variable "validación", de una comparación (==) entre la nota original (variable "nota") con un valor númerico nuevo.
+validación2 = nota != 5.5 #Lo mismo que el anterior, solo que aquí se busca saber si no es igual (si no es igual dará como resultado true)
+
+
+estado = nota > aprobación
+
+grad = year + 5 
+
+print(grad)
+print(temperature)
+print(pi)
+print(validación)
+print(validación2)"""
+
+edad = 20 #Variable de tipo entero
+temperatura = 27.5 #Variable del tipo flotante
+pi = 3.14159
+pi_entero = int(pi) #entero no tiene decimales, mientras que el flotante si. Hago un cambio de tipo de dato a entero
+nombre = "José" #Variable del tipo númerico
+
+print(pi_entero)
