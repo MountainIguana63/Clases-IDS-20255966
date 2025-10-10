6f4b03554f84046f26f2c4b9e8e22a0deef60e90
VicS✩l =) <vsolano.ram@gmail.com>
2025-10-10 09:25:44 -0600

    Semana 4
    Me puse al día con las clases de la Semana 4

---
+++ Semana4_IDS/02_10_25_IDS/clase 04 - 03.py
@@ -0,0 +1,19 @@
+"""ItemIngresar = input("Ingrese el item: ")
+print(f"El item ingresado es {ItemIngresar}")"""
+
+my_string = "ABCDEFGHI" # String, cadena de caráctares
+"""letra= int(input("Indique el indice de la letra a mostrar (del 0 al 4): "))
+print(my_string[letra])"""
+
+#print(len(my_string))
+#print(my_string[1:5:50]), para subsetting por slices
+
+#print("O" not in my_string)
+
+#print("AJP"*8)
+
+#print(min(my_string))
+#print(max(my_string))
+print(my_string.count("C"))
+
+print(my_string.count(" "))
---
+++ Semana4_IDS/30_09_25_IDS/Clase 04 - 01.py
@@ -0,0 +1,31 @@
+"""usuario = "Javier"
+cantidad_alumnos = 74
+media_edad = 18.231234
+monto_hope = 1234.5678901
+inversión_evento = -987654.215448
+
+print(type(cantidad_alumnos))
+print(type(media_edad))
+
+print(type(cantidad_alumnos) is int) # no es lo mismo "int"
+print(type(media_edad) is int)
+
+print("el usuario es", usuario, "y tiene", cantidad_alumnos, "pajaritos en su aula")
+print("y la edad promedio es de", media_edad)
+
+print(f"El usuario es {usuario}")
+print(f"y en su aula con {cantidad_alumnos - 4} pajaritos")
+print(f"con edad promedio de {media_edad:.2f} años")
+print(f"{monto_hope:,.2f} como donativo")
+print(f"y la todalidad de gastos fue de ${abs(inversión_evento):,.2f} dolares")
+
+print(type(usuario)is str)
+
+esta_lloviendo = False
+print(type(esta_lloviendo) is not bool)
+print(type(monto_hope) is not bool)"""
+
+nombre = "Alvin"
+apellido = "Portillo"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
